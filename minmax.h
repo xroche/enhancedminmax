d54c9277ea829669d75f49c02c14c8ce2777de16
Xavier Roche <xavier.roche@algolia.com>
2020-03-01 21:07:19 +0100

    Use types to propagate signedness change flag

--- minmax.h
+++ minmax.h
@@ -65,7 +65,7 @@ struct lower_than
     }
 };
 
-/** Helper: return @c true if r is strictly higher than l ***/
+/** Helper: return @c true if l is strictly higher than r ***/
 struct higher_than : private lower_than
 {
     template<typename L, typename R>
@@ -76,54 +76,87 @@ struct higher_than : private lower_than
     }
 };
 
+/** Signed valued encapsulated into a unsigned value. **/
+template<typename Unsigned>
+struct signed_in_unsigned_value
+{
+    static_assert(std::is_integral_v<Unsigned>);
+    static_assert(std::is_unsigned_v<Unsigned>);
+
+    using type = Unsigned;
+    using originalType = std::make_signed_t<std::decay_t<Unsigned>>;
+
+    constexpr inline signed_in_unsigned_value(Unsigned value, bool is_signed)
+      : value(value)
+      , is_signed(is_signed)
+    {}
+
+    constexpr inline auto getOriginal() { return static_cast<originalType>(value); }
+
+    Unsigned value{};
+    bool is_signed = false;
+};
+template<typename T>
+struct is_signed_in_unsigned_value : std::false_type
+{};
+template<typename T>
+struct is_signed_in_unsigned_value<signed_in_unsigned_value<T>> : std::true_type
+{};
+static_assert(!is_signed_in_unsigned_value<int>::value);
+static_assert(is_signed_in_unsigned_value<signed_in_unsigned_value<unsigned>>::value);
+template<typename T>
+inline constexpr bool is_signed_in_unsigned_value_v = is_signed_in_unsigned_value<T>::value;
+
 /**
  * Return the maximum of a set of values. If the values are compatible, return the lvalue-reference, or
  * rvalue-reference, depending on the case.
  * @param less The comparison functor
- * @param is_signed Set to @c true if the selected value was originally a signed integral type; even if the
- * returned type is unsigned (because 'true ? -1 : 1' yields an unsigned type)
- * @return The lowest value, which may be an integral signed type value cast to unsigned if is_signed is set to @c
- * true.
+ * @return The lowest value, which may be an integral signed type value cast to unsigned if the type matches @c
+ * is_signed_in_unsigned_value.
  **/
 template<typename Compare, typename L, typename... R>
-decltype(auto) inline constexpr find_lowest(const Compare& less, bool& is_signed, L&& l, R&&... r)
+decltype(auto) inline constexpr find_lowest(const Compare& less, L&& l, R&&... r)
 {
     // Only one element to compare: return it.
     if constexpr (sizeof...(r) == 0) {
-        // If handling integral types, propagate sign
-        if constexpr (std::is_integral_v<L>) {
-            is_signed = std::is_signed_v<L>;
-        }
         return std::forward<L>(l);
     } else {
         // Find lowest of the right part, and return the sign.
-        bool right_signed = false;
-        auto&& right = find_lowest(less, right_signed, std::forward<R>(r)...);
+        auto&& right = find_lowest(less, std::forward<R>(r)...);
+        using RightType = std::decay_t<decltype(right)>;
 
         // The right part is an unsigned integral type, but was originally a signed value. Compare it with the
-        // right type then.
-        using RightType = std::decay_t<decltype(right)>;
-        if constexpr (std::is_integral_v<RightType> && std::is_unsigned_v<RightType>) {
-            if (right_signed) {
-                using RealRightType = std::make_signed_t<RightType>;
-                auto&& realRight = static_cast<RealRightType>(right);
+        // original type then. We'll propagate the flag telling the upstream caller that the type had signedness
+        // changed at some point.
+        if constexpr (is_signed_in_unsigned_value_v<RightType>) {
+            static_assert(std::is_unsigned_v<typename RightType::type>);
+            static_assert(std::is_signed_v<typename RightType::originalType>);
 
-                // Compare it with the right type
-                const bool is_lower = less(std::forward<L>(l), realRight);
-                is_signed = is_lower ? std::is_signed_v<L> : right_signed;
+            // Compare it with the right type if signed bit is set, but return the unsigned one
+            const bool is_lower = right.is_signed ? less(std::forward<L>(l), right.getOriginal())
+                                                  : less(std::forward<L>(l), right.value);
+            auto&& value = is_lower ? std::forward<L>(l) : right.value;
+            const bool is_signed = is_lower ? std::is_signed_v<L> : right.is_signed;
+            static_assert(std::is_unsigned_v<std::decay_t<decltype(value)>>);
 
-                // But return the expected type
+            // Propagate the correct signedness
+            return signed_in_unsigned_value(value, is_signed);
+        } else {
+            // Compare the left and right part.
+            const bool is_lower = less(std::forward<L>(l), right);
+
+            // If we change signedness, encapsulate the return value in a signed_in_unsigned_value().
+            // For that, introspect the return type first, before we compute the value.
+            if constexpr (std::is_integral_v<L> && std::is_integral_v<RightType> &&
+                          std::is_signed_v<L> != std::is_signed_v<RightType>) {
+                static_assert(std::is_unsigned_v<std::decay_t<decltype(is_lower ? std::forward<L>(l) : right)>>);
+                const bool is_signed = is_lower ? std::is_signed_v<L> : std::is_signed_v<RightType>;
+                return signed_in_unsigned_value(is_lower ? std::forward<L>(l) : right, is_signed);
+            } else {
+                // Otherwise just forward the value.
                 return is_lower ? std::forward<L>(l) : right;
             }
         }
-
-        // Compare and return the value. Mark sign flag if needed for upstream to handle further comparisons.
-        const bool is_lower = less(std::forward<L>(l), right);
-        // If handling integral types, propagate sign
-        if constexpr (std::is_integral_v<L>) {
-            is_signed = is_lower ? std::is_signed_v<L> : right_signed;
-        }
-        return is_lower ? std::forward<L>(l) : right;
     }
 }
 
@@ -132,8 +165,14 @@ decltype(auto) inline constexpr find_lowest(const Compare& less, bool& is_signed
 template<typename... T>
 decltype(auto) inline constexpr min(T&&... args)
 {
-    bool is_signed = false;
-    return find_lowest(lower_than(), is_signed, std::forward<T>(args)...);
+    // Introspect the return type first, before we compute the value.
+    using ValueType = std::decay_t<decltype(find_lowest(lower_than(), std::forward<T>(args)...))>;
+    if constexpr (is_signed_in_unsigned_value_v<ValueType>) {
+        auto&& value = find_lowest(lower_than(), std::forward<T>(args)...);
+        return value.value;
+    } else {
+        return find_lowest(lower_than(), std::forward<T>(args)...);
+    }
 }
 
 /** Return the maximum of a set of values. If the values are compatible, return the lvalue-reference, or
@@ -141,8 +180,14 @@ decltype(auto) inline constexpr min(T&&... args)
 template<typename... T>
 decltype(auto) inline constexpr max(T&&... args)
 {
-    bool is_signed = false;
-    return find_lowest(higher_than(), is_signed, std::forward<T>(args)...);
+    // Introspect the return type first, before we compute the value.
+    using ValueType = std::decay_t<decltype(find_lowest(higher_than(), std::forward<T>(args)...))>;
+    if constexpr (is_signed_in_unsigned_value_v<ValueType>) {
+        auto&& value = find_lowest(higher_than(), std::forward<T>(args)...);
+        return value.value;
+    } else {
+        return find_lowest(higher_than(), std::forward<T>(args)...);
+    }
 }
 
 /* Unit tests */
@@ -163,6 +208,7 @@ static_assert(max(0, 1u) == 1);
 static_assert(max(1, 0u) == 1);
 static_assert(max(0, -1) == 0);
 static_assert(std::is_signed_v<decltype(min(0, -1))>);
+static_assert(max(0, 1, 2, 3, 4, 5, -1, -2, -3, 1) == 5);
 static_assert(max(-1, 0) == 0);
 static_assert(max(0u, -1) == 0);
 static_assert(std::is_unsigned_v<decltype(min(0u, -1))>);
@@ -176,7 +222,12 @@ static_assert(max(-2, 0u, 7u) == 7);
 static_assert(max(0, 7u, -2) == 7);
 static_assert(max(0u, 7u, -2) == 7);
 static_assert(max(7u, 0u, -2) == 7);
-static_assert(max(std::numeric_limits<int>::min(), 0, 7) == 7);
+static_assert(max(std::numeric_limits<int>::min(), 0u, 7) == 7);
+static_assert(max(1u, -1, std::numeric_limits<unsigned>::max()) == std::numeric_limits<unsigned>::max());
+static_assert(max(-1, 1u, std::numeric_limits<unsigned>::max()) == std::numeric_limits<unsigned>::max());
+static_assert(max(1u, std::numeric_limits<unsigned>::max(), -1) == std::numeric_limits<unsigned>::max());
+static_assert(max(std::numeric_limits<unsigned>::max(), 1u, -1) == std::numeric_limits<unsigned>::max());
+static_assert(max(-1, std::numeric_limits<int>::max(), -3, 2u, -7, 7u, 2u, 0u) == std::numeric_limits<int>::max());
 static_assert(max(std::numeric_limits<unsigned long long>::min(), 0, 7) == 7);
 static_assert(max(std::numeric_limits<signed long long>::min(), 0, 7) == 7);
 static_assert(max(std::numeric_limits<signed long long>::max(), 0, 7) ==
@@ -213,16 +264,26 @@ static_assert(min(0u, -2) == -2);
 static_assert(min(2u, 0, 7u) == 0);
 static_assert(min(2u, 0u, 7u) == 0);
 static_assert(min(-2, 0u) == -2);
+static_assert(min(0, -2, 1, 2, 3, -2, 4, -2, 5, -1, -2, -3, 1, 2, 20) == -3);
 
 static_assert(min(-2, 0u, 7u) == -2);
 static_assert(min(0u, 7u, -2) == -2);
 static_assert(min(7u, 0u, -2) == -2);
 static_assert(min(-2, 0u, 7u) == unsigned(-2));
+static_assert(min(-1, std::numeric_limits<int>::max(), -3, 2u, -7, 7u, 2u, 0u) == -7);
 static_assert(std::is_unsigned_v<decltype(min(-2, 0u, 7u))>);
 static_assert(min(0u, 7u, -2) == unsigned(-2));
 static_assert(min(7u, 0u, -2) == unsigned(-2));
 static_assert(min(-2, -1, -7) == -7);
 static_assert(std::is_signed_v<decltype(min(-2, -1, -7))>);
+static_assert(min(1u, -7, std::numeric_limits<unsigned>::max()) == -7);
+static_assert(min(-7, 1u, std::numeric_limits<unsigned>::max()) == -7);
+static_assert(min(1u, std::numeric_limits<unsigned>::max(), -7) == -7);
+static_assert(min(std::numeric_limits<unsigned>::max(), 1u, -7) == -7);
+static_assert(min(1u, -7, -1, std::numeric_limits<unsigned>::max()) == -7);
+static_assert(min(-7, 1u, -1, std::numeric_limits<unsigned>::max()) == -7);
+static_assert(min(1u, std::numeric_limits<unsigned>::max(), -1, -7) == -7);
+static_assert(min(std::numeric_limits<unsigned>::max(), 1u, -7) == -7);
 static_assert(min(std::numeric_limits<int>::min(), 0, 7) == std::numeric_limits<int>::min());
 static_assert(min(std::numeric_limits<unsigned long long>::min(), 0, 7) ==
               std::numeric_limits<unsigned long long>::min());
@@ -307,7 +368,7 @@ inline constexpr auto ut1()
     std::size_t a = 42;
     std::size_t b = 100;
     auto&& v = max(a, b);
-    static_assert(std::is_lvalue_reference_v<decltype(v)>);
+    static_assert(std::is_same_v<decltype(v), std::size_t&>);
     return ++v;
 }
 static_assert(ut1() == 101);
@@ -316,7 +377,9 @@ inline constexpr auto ut2()
 {
     std::size_t a = 42;
     const int b = 100;
-    return max(a, b);
+    auto&& v = max(a, b);
+    static_assert(std::is_same_v<std::decay_t<decltype(v)>, std::size_t>);
+    return v;
 }
 static_assert(ut2() == 100);
 
@@ -329,7 +392,7 @@ inline constexpr auto ut3()
     std::size_t e = 45;
     std::size_t f = 78;
     auto&& v = max(a, b, c, d, e, f);
-    static_assert(std::is_lvalue_reference_v<decltype(v)>);
+    static_assert(std::is_same_v<decltype(v), std::size_t&>);
     return ++v;
 }
 static_assert(ut3() == 101);
@@ -375,17 +438,45 @@ static_assert(ut7() == 100 - 1);
 
 static_assert(max(string("42"), string("100"), string("0"), string("39")).integer() == 100);
 
-inline std::size_t ut8(std::size_t a = 42,
-                       std::size_t b = 100,
-                       std::size_t c = 1,
-                       std::size_t d = 3,
-                       std::size_t e = 45,
-                       std::size_t f = 78)
+inline constexpr std::size_t ut8(std::size_t a = 42,
+                                 std::size_t b = 100,
+                                 std::size_t c = 1,
+                                 std::size_t d = 3,
+                                 std::size_t e = 45,
+                                 std::size_t f = 78)
 {
     auto&& v = max(a, b, c, d, e, f);
-    static_assert(std::is_lvalue_reference_v<decltype(v)>);
+    static_assert(std::is_same_v<decltype(v), std::size_t&>);
     return ++v;
 }
+static_assert(ut8() == 101);
+
+inline constexpr auto ut9()
+{
+    std::size_t a = 42;
+    std::size_t b = 100;
+    const std::size_t c = 1;
+    std::size_t d = 3;
+    std::size_t e = 45;
+    std::size_t f = 78;
+    auto&& v = max(a, b, c, d, e, f);
+    static_assert(std::is_same_v<decltype(v), const std::size_t&>);
+    return v;
+}
+static_assert(ut9() == 100);
+
+inline constexpr std::size_t ut10(std::size_t a = 42,
+                                  std::size_t b = 100,
+                                  const std::size_t c = 1,
+                                  std::size_t d = 3,
+                                  std::size_t e = 45,
+                                  std::size_t f = 78)
+{
+    auto&& v = max(a, b, c, d, e, f);
+    static_assert(std::is_same_v<decltype(v), const std::size_t&>);
+    return v;
+}
+static_assert(ut10() == 100);
 
 } // namespace ut
 
